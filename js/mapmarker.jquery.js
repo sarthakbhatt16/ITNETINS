17e4ebeb1c3d3f12f762e23d2ed9b1dd2c369ff8
Nikhil Deshmukh <nd281092@gmail.com>
2019-07-24 21:59:25 -0400

    course detail page for ba added and address updated

--- js/mapmarker.jquery.js
+++ js/mapmarker.jquery.js
@@ -1,5 +1,5 @@
-(function($){
-	$.fn.mapmarker = function(options){
+(function($) {
+	$.fn.mapmarker = function(options) {
 		var opts = $.extend({}, $.fn.mapmarker.defaults, options);
 
 		return this.each(function() {
@@ -11,55 +11,54 @@
 
 	// Set up default values
 	var defaultMarkers = {
-		"markers": []
+		markers: []
 	};
 
 	$.fn.mapmarker.defaults = {
-		zoom	: 8,
-		center	: 'United States',
-		markers	: defaultMarkers
-	}
+		zoom: 8,
+		center: 'United States',
+		markers: defaultMarkers
+	};
 
 	// Main function code here (ref:google map api v3)
-	function addMapMarker(map_element, zoom, center, markers){
+	function addMapMarker(map_element, zoom, center, markers) {
 		//console.log($.fn.mapmarker.defaults['center']);
 
 		//Set center of the Map
 		var myOptions = {
-		  zoom: zoom,
-		  mapTypeId: google.maps.MapTypeId.ROADMAP
-		}
+			zoom: zoom,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
 		var map = new google.maps.Map(map_element, myOptions);
 		var geocoder = new google.maps.Geocoder();
 		var infowindow = null;
-		var baloon_text = "";
+		var baloon_text = '';
 
-		geocoder.geocode( { 'address': center}, function(results, status) {
+		geocoder.geocode({ address: center }, function(results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
 				//In this case it creates a marker, but you can get the lat and lng from the location.LatLng
 				map.setCenter(results[0].geometry.location);
-			}
-			else{
-				console.log("Geocode was not successful for the following reason: " + status);
+			} else {
+				console.log('Geocode was not successful for the following reason: ' + status);
 			}
 		});
 
 		//run the marker JSON loop here
-		$.each(markers.markers, function(i, the_marker){
-			latitude=the_marker.latitude;
-			longitude=the_marker.longitude;
-			icon=the_marker.icon;
-			var baloon_text=the_marker.baloon_text;
+		$.each(markers.markers, function(i, the_marker) {
+			latitude = the_marker.latitude;
+			longitude = the_marker.longitude;
+			icon = the_marker.icon;
+			var baloon_text = the_marker.baloon_text;
 
-			if(latitude!="" && longitude!=""){
+			if (latitude != '' && longitude != '') {
 				var marker = new google.maps.Marker({
-					map: map, 
-					position: new google.maps.LatLng(latitude,longitude),
+					map: map,
+					position: new google.maps.LatLng(latitude, longitude),
 					animation: google.maps.Animation.DROP,
 					icon: icon
 				});
 
-				// Set up markers with info windows 
+				// Set up markers with info windows
 				google.maps.event.addListener(marker, 'click', function() {
 					// Close all open infowindows
 					if (infowindow) {
@@ -70,10 +69,9 @@
 						content: baloon_text
 					});
 
-					infowindow.open(map,marker);
+					infowindow.open(map, marker);
 				});
 			}
 		});
 	}
-
 })(jQuery);
